40b735eefd67b49b478b5bfcf449b8d0b6b07726
Martin Schoeberl <martin@jopdesign.com>
2018-11-22 17:53:10 -0800

    More exercises, sort out Hello World naming

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,3 +1,45 @@
-object Hello extends App{
-  println("Hello Chisel World!")
+/*
+ * This code is a minimal hardware described in Chisel.
+ * 
+ * Copyright: 2013, Technical University of Denmark, DTU Compute
+ * Author: Martin Schoeberl (martin@jopdesign.com)
+ * License: Simplified BSD License
+ * 
+ * Blinking LED: the FPGA version of Hello World
+ */
+
+//- start import
+import chisel3._
+//- end
+
+/**
+ * The blinking LED component.
+ */
+
+//- start hello
+class Hello extends Module {
+  val io = IO(new Bundle {
+    val led = Output(UInt(1.W))
+  })
+  val CNT_MAX = (50000000 / 2 - 1).U;
+  
+  val cntReg = RegInit(0.U(32.W))
+  val blkReg = RegInit(0.U(1.W))
+
+  cntReg := cntReg + 1.U
+  when(cntReg === CNT_MAX) {
+    cntReg := 0.U
+    blkReg := ~blkReg
+  }
+  io.led := blkReg
 }
+//- end
+
+/**
+ * An object extending App to generate the Verilog code.
+ */
+//- start generate
+object Hello extends App {
+  chisel3.Driver.execute(Array[String](), () => new Hello())
+}
+//- end
