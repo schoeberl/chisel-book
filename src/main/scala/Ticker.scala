d935bdeee62f5f83c62ca32ab8ea1dafd80b4dc4
Martin Schoeberl <martin@jopdesign.com>
2019-08-21 22:57:45 +0200

    Fix inheritance tester, start with inheritance example

---
+++ src/main/scala/Ticker.scala
@@ -0,0 +1,24 @@
+import chisel3._
+
+abstract class Ticker(n: Int) extends Module {
+  val io = IO(new Bundle{
+    val tick = Output(Bool())
+  })
+
+//  val cntReg = Reg(UInt(8.W))
+}
+
+class WhenTicker(n: Int) extends Ticker(n) {
+
+  val N = (n-1).U
+
+
+  val cntReg = RegInit(0.U(8.W))
+
+  cntReg := cntReg + 1.U
+  when(cntReg === N) {
+    cntReg := 0.U
+  }
+
+  io.tick := cntReg === N
+}
--- src/test/scala/CounterSpec.scala
+++ src/test/scala/CounterSpec.scala
@@ -1,7 +1,7 @@
 import chisel3.iotesters.PeekPokeTester
 import org.scalatest._
 
-class CounterTester(c: Counter, n: Int) extends PeekPokeTester(c: Counter) {
+class CounterTester[T <: Counter](c: T, n: Int) extends PeekPokeTester(c: T) {
 
   var count = -1
   for (i <- 0 to n * 3) {
@@ -25,44 +25,38 @@ class CounterTester(c: Counter, n: Int) extends PeekPokeTester(c: Counter) {
 
 class CounterSpec extends FlatSpec with Matchers {
 
-  def getWhenCounter(n: Int): Counter = new WhenCounter(n)
-  def getMuxCounter(n: Int): Counter = new MuxCounter(n)
-  def getDownCounter(n: Int): Counter = new DownCounter(n)
-  def getFunctionCounter(n: Int): Counter = new FunctionCounter(n)
-  def getNerdCounter(n: Int): Counter = new NerdCounter(n)
-
   "WhenCounter 4" should "pass" in {
-    chisel3.iotesters.Driver(() => getWhenCounter(4)) { c =>
+    chisel3.iotesters.Driver(() => new WhenCounter(4)) { c =>
       new CounterTester(c, 4)
     } should be (true)
   }
 
   "WhenCounter 7" should "pass" in {
-    chisel3.iotesters.Driver(() => getWhenCounter(7)) { c =>
+    chisel3.iotesters.Driver(() => new WhenCounter(7)) { c =>
       new CounterTester(c, 7)
     } should be (true)
   }
 
   "MuxCounter 5" should "pass" in {
-    chisel3.iotesters.Driver(() => getMuxCounter(5)) { c =>
+    chisel3.iotesters.Driver(() => new MuxCounter(5)) { c =>
       new CounterTester(c, 5)
     } should be (true)
   }
 
   "DownCounter 7" should "pass" in {
-    chisel3.iotesters.Driver(() => getDownCounter(7)) { c =>
+    chisel3.iotesters.Driver(() => new DownCounter(7)) { c =>
       new CounterTester(c, 7)
     } should be (true)
   }
 
   "FunctionCounter 8" should "pass" in {
-    chisel3.iotesters.Driver(() => getFunctionCounter(8)) { c =>
+    chisel3.iotesters.Driver(() => new FunctionCounter(8)) { c =>
       new CounterTester(c, 8)
     } should be (true)
   }
 
   "NerdCounter 13" should "pass" in {
-    chisel3.iotesters.Driver(() => getNerdCounter(13)) { c =>
+    chisel3.iotesters.Driver(() => new NerdCounter(13)) { c =>
       new CounterTester(c, 13)
     } should be (true)
   }
