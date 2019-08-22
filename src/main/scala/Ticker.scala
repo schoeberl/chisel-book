19b74758cb355e724a3efe528b0432f097ff5698
Martin Schoeberl <martin@jopdesign.com>
2019-08-22 12:27:34 +0200

    Finish inheritance with the ticker example

--- src/main/scala/Ticker.scala
+++ src/main/scala/Ticker.scala
@@ -5,16 +5,13 @@ abstract class Ticker(n: Int) extends Module {
   val io = IO(new Bundle{
     val tick = Output(Bool())
   })
-
-//  val cntReg = Reg(UInt(8.W))
 }
 //- end
 
-//- start when_ticker
-class WhenTicker(n: Int) extends Ticker(n) {
+//- start up_ticker
+class UpTicker(n: Int) extends Ticker(n) {
 
   val N = (n-1).U
-
 
   val cntReg = RegInit(0.U(8.W))
 
@@ -24,5 +21,38 @@ class WhenTicker(n: Int) extends Ticker(n) {
   }
 
   io.tick := cntReg === N
+}
+//- end
+
+//- start down_ticker
+class DownTicker(n: Int) extends Ticker(n) {
+
+  val N = (n-1).U
+
+  val cntReg = RegInit(N)
+
+  cntReg := cntReg - 1.U
+  when(cntReg === 0.U) {
+    cntReg := N
+  }
+
+  io.tick := cntReg === N
+}
+//- end
+
+//- start nerd_ticker
+class NerdTicker(n: Int) extends Ticker(n) {
+
+  val N = n
+
+  val MAX = (N - 2).S(8.W)
+  val cntReg = RegInit(MAX)
+  io.tick := false.B
+
+  cntReg := cntReg - 1.S
+  when(cntReg(7)) {
+    cntReg := MAX
+    io.tick := true.B
+  }
 }
 //- end
--- src/test/scala/TickerSpec.scala
+++ src/test/scala/TickerSpec.scala
@@ -2,17 +2,20 @@
 import chisel3.iotesters.PeekPokeTester
 import org.scalatest._
 
-class TickerTester[T <: Ticker](c: T, n: Int) extends PeekPokeTester(c: T) {
+class TickerTester[T <: Ticker](dut: T, n: Int) extends PeekPokeTester(dut: T) {
 
+  // -1 is the notion that we have not yet seen the first tick
   var count = -1
   for (i <- 0 to n * 3) {
     if (count > 0) {
-      expect(c.io.tick, 0)
+      expect(dut.io.tick, 0)
     }
     if (count == 0) {
-      expect(c.io.tick, 1)
+      expect(dut.io.tick, 1)
     }
-    val t = peek(c.io.tick)
+    val t = peek(dut.io.tick)
+    // On a tick we reset the tester counter to N-1,
+    // otherwise we decrement the tester counter
     if (t == 1) {
       count = n-1
     } else {
@@ -24,13 +27,26 @@ class TickerTester[T <: Ticker](c: T, n: Int) extends PeekPokeTester(c: T) {
 }
 //- end
 
-
+//- start ticker_spec
 class TickerSpec extends FlatSpec with Matchers {
 
-  "WhenTicker 5" should "pass" in {
-    chisel3.iotesters.Driver(() => new WhenTicker(5)) { c =>
+  "UpTicker 5" should "pass" in {
+    chisel3.iotesters.Driver(() => new UpTicker(5)) { c =>
       new TickerTester(c, 5)
     } should be (true)
   }
+
+  "DownTicker 7" should "pass" in {
+    chisel3.iotesters.Driver(() => new DownTicker(7)) { c =>
+      new TickerTester(c, 7)
+    } should be (true)
+  }
+
+  "NerdTicker 11" should "pass" in {
+    chisel3.iotesters.Driver(() => new NerdTicker(11)) { c =>
+      new TickerTester(c, 11)
+    } should be (true)
+  }
 }
+//- end
 
