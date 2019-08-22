9cbd2f177531f02621a4c4a911bd718e75196316
Martin Schoeberl <martin@jopdesign.com>
2019-08-22 13:15:14 +0200

    File reading

---
+++ src/main/scala/FileReader.scala
@@ -0,0 +1,27 @@
+//- start file_reader
+import chisel3._
+import scala.io.Source
+
+class FileReader extends Module {
+  val io = IO(new Bundle {
+    val address = Input(UInt(8.W))
+    val data = Output(UInt(8.W))
+  })
+
+  val array = new Array[Int](256)
+  var idx = 0
+
+  // read the data into a Scala array
+  val source = Source.fromFile("data.txt")
+  for (line <- source.getLines()) {
+    array(idx) = line.toInt
+    idx += 1
+  }
+
+  // convert the Scala integer array into the Chisel type Vec
+  val table = VecInit(array.map(_.U(8.W)))
+
+  // use the table
+  io.data := table(io.address)
+}
+//- end
