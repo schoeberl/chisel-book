d0a90101c807bea67c8fd656884e8287ea7bf8da
Martin Schoeberl <martin@jopdesign.com>
2019-09-25 15:28:40 +0200

    The double buffer FIFO

--- src/main/scala/fifo.scala
+++ src/main/scala/fifo.scala
@@ -2,14 +2,19 @@
 // to avoid name clashes
 package fifo
 
-import Chisel._
+import chisel3._
+import chisel3.util._
+
+//- start fifo_io
+class FifoIO[T <: Data](private val gen: T) extends Bundle {
+  val enq = Flipped(new DecoupledIO(gen))
+  val deq = new DecoupledIO(gen)
+}
+//- end
 
 //- start fifo_abstract
-abstract class Fifo[T <: Data](gen: T) extends Module {
-  val io = new Bundle {
-    val enq = Flipped(new DecoupledIO(gen))
-    val deq = new DecoupledIO(gen)
-  }
+abstract class Fifo[T <: Data](gen: T, depth: Int) extends Module {
+  val io = IO(new FifoIO(gen))
 }
 //- end
 
@@ -24,3 +29,94 @@ package fifo_private {
   }
   //- end
 }
+
+
+
+//- start fifo_bubble
+class BubbleFifo[T <: Data](gen: T, depth: Int) extends Fifo(gen: T, depth: Int) {
+
+  private class Buffer[T <: Data](gen: T) extends Module {
+    val io = IO(new FifoIO(gen))
+
+    val fullReg = RegInit(false.B)
+    val dataReg = Reg(gen)
+
+    when (fullReg) {
+      when (io.deq.ready) {
+        fullReg := false.B
+      }
+    } .otherwise {
+      when (io.enq.valid) {
+        fullReg := true.B
+        dataReg := io.enq.bits
+      }
+    }
+
+    io.enq.ready := !fullReg
+    io.deq.valid := fullReg
+    io.deq.bits := dataReg
+  }
+
+  private val buffers = Array.fill(depth) { Module(new Buffer(gen)) }
+  for (i <- 0 until depth - 1) {
+    buffers(i + 1).io.enq <> buffers(i).io.deq
+  }
+
+  io.enq <> buffers(0).io.enq
+  io.deq <> buffers(depth - 1).io.deq
+}
+//- end
+
+//- start fifo_double_buffer
+class DoubleBufferFifo[T <: Data](gen: T, depth: Int) extends Fifo(gen: T, depth: Int) {
+
+  private class DoubleBuffer[T <: Data](gen: T) extends Module {
+    val io = IO(new FifoIO(gen))
+
+    val empty :: one :: two :: Nil = Enum(3)
+    val stateReg = RegInit(empty)
+    val dataReg = Reg(gen)
+    val shadowReg = Reg(gen)
+
+    switch(stateReg) {
+      is (empty) {
+        when (io.enq.valid) {
+          stateReg := one
+          dataReg := io.enq.bits
+        }
+      }
+      is (one) {
+        when (io.deq.ready && !io.enq.valid) {
+          stateReg := empty
+        }
+        when (io.deq.ready && io.enq.valid) {
+          stateReg := one
+          dataReg := io.enq.bits
+        }
+        when (!io.deq.ready && io.enq.valid) {
+          stateReg := two
+          shadowReg := io.enq.bits
+        }
+      }
+      is (two) {
+        when (io.deq.ready) {
+          dataReg := shadowReg
+          stateReg := one
+        }
+      }
+    }
+    io.enq.ready := (stateReg === empty || stateReg === one)
+    io.deq.valid := (stateReg === one || stateReg === two)
+    io.deq.bits := dataReg
+  }
+  //- end
+
+  private val buffers = Array.fill(depth) { Module(new DoubleBuffer(gen)) }
+  for (i <- 0 until depth - 1) {
+    buffers(i + 1).io.enq <> buffers(i).io.deq
+  }
+
+  io.enq <> buffers(0).io.enq
+  io.deq <> buffers(depth - 1).io.deq
+}
+
---
+++ src/test/scala/FifoSpec.scala
@@ -0,0 +1,97 @@
+package fifo
+
+import chisel3._
+import chisel3.iotesters.PeekPokeTester
+import org.scalatest._
+
+class FifoTester[T <: Fifo[_ <: Data]](dut: T) extends PeekPokeTester(dut) {
+// class FifoTester[D <: Data, T[_ <: Data] <: Fifo[_]](dut: T[D]) extends PeekPokeTester(dut) {
+// class FifoTester(dut: BubbleFifo[UInt]) extends PeekPokeTester(dut) {
+
+  // some defaults for all signals
+  poke(dut.io.enq.bits.asUInt(), 0xab)
+  poke(dut.io.enq.valid, 0)
+  poke(dut.io.deq.ready, 0)
+  step(1)
+  var ready = peek(dut.io.enq.ready)
+  var valid = peek(dut.io.deq.valid)
+  // write one value and expect it on the deq side
+  poke(dut.io.enq.bits.asUInt(), 0x123)
+  poke(dut.io.enq.valid, 1)
+  step(1)
+  poke(dut.io.enq.bits.asUInt(), 0xab)
+  poke(dut.io.enq.valid, 0)
+  step(12)
+  expect(dut.io.enq.ready, 1)
+  expect(dut.io.deq.valid, 1)
+  expect(dut.io.deq.bits.asUInt(), 0x123)
+  // read it out
+  poke(dut.io.deq.ready, 1)
+  step(1)
+  expect(dut.io.deq.valid, 0)
+  poke(dut.io.deq.ready, 0)
+  step(1)
+  // file the whole buffer
+  // TODO: how do I access the parameter depth from the dut?
+  var cnt = 1
+  // we are always valid
+  poke(dut.io.enq.valid, 1)
+  for (i <- 0 until 12) {
+    poke(dut.io.enq.bits.asUInt(), cnt.U)
+    if (peek(dut.io.enq.ready) != 0) {
+      cnt += 1
+    }
+    step(1)
+  }
+  println(s"Wrote ${cnt-1} words")
+  expect(dut.io.enq.ready, 0)
+  expect(dut.io.deq.valid, 1)
+  expect(dut.io.deq.bits.asUInt(), 1)
+
+  // now read it back
+  var expected = 1
+  // no new input
+  poke(dut.io.enq.valid, 0)
+  // always ready on the reader side
+  poke(dut.io.deq.ready, 1)
+  for (i <- 0 until 12) {
+    if (peek(dut.io.deq.valid) != 0) {
+      expect(dut.io.deq.bits.asUInt(), expected)
+      expected += 1
+    }
+    step(1)
+  }
+
+  // now do the speed test
+  poke(dut.io.enq.valid, 1)
+  poke(dut.io.deq.ready, 1)
+  cnt = 0
+  for (i <- 0 until 100) {
+    poke(dut.io.enq.bits.asUInt(), i.U)
+    if (peek(dut.io.enq.ready) != 0) {
+      cnt += 1
+    }
+    step(1)
+  }
+  val cycles = 100.0 / cnt
+  println(s"$cnt words in 100 clock cycles, $cycles clock cycles per word")
+  assert(cycles >= 0.99, "Cannot be faster than one clock cycle per word")
+}
+
+class FifoSpec extends FlatSpec with Matchers {
+
+  "BubbleFifo" should "pass" in {
+    chisel3.iotesters.Driver.execute(Array("--target-dir", "generated", "--fint-write-vcd"),
+      () => new BubbleFifo(UInt(16.W), 4)) { c =>
+      new FifoTester(c)
+    } should be (true)
+  }
+
+  "DoubleBufferFifo" should "pass" in {
+    chisel3.iotesters.Driver.execute(Array("--target-dir", "generated", "--fint-write-vcd"),
+      () => new DoubleBufferFifo(UInt(16.W), 4)) { c =>
+      new FifoTester(c)
+    } should be (true)
+  }
+}
+
