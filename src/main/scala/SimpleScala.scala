2bccd41699d139a42505af687f404e649a2a0e09
Martin Schoeberl <martin@jopdesign.com>
2020-03-31 18:29:28 +0200

    Get started with some Scala introduction

--- src/main/scala/SimpleScala.scala
+++ src/main/scala/SimpleScala.scala
@@ -1,3 +1,28 @@
 class SimpleScala {
 
+  /*
+  //- start scala_val
+  // A value is a constant
+  val zero = 0
+  // No new assignment; this will not compile
+  zero = 3
+  //- end
+  */
+
+  //- start scala_var
+  // We can change the value os a var variable
+  var x = 2
+  x = 3
+  //- end
+
+  //- start scala_int_type
+  val number: Int = 42
+  //- end
+
+  var v = "Hello"
+  v = "Hello World"
+
+  // Type usually inferred, but can be declared
+  var s: String = "abc"
+
 }
