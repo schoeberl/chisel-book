7e0335f88de81218db909e0af0460d1a9084c755
Martin Schoeberl <martin@jopdesign.com>
2020-04-01 14:49:36 +0200

    Some more Scala

--- src/main/scala/SimpleScala.scala
+++ src/main/scala/SimpleScala.scala
@@ -1,16 +1,21 @@
-class SimpleScala {
+import chisel3._
+
+class SimpleScala extends Module {
+
+  val io = new Bundle() {}
 
   /*
   //- start scala_val
   // A value is a constant
   val zero = 0
-  // No new assignment; this will not compile
+  // No new assignment is possible
+  // The following will not compile
   zero = 3
   //- end
   */
 
   //- start scala_var
-  // We can change the value os a var variable
+  // We can change the value of a var variable
   var x = 2
   x = 3
   //- end
@@ -25,4 +30,33 @@ class SimpleScala {
   // Type usually inferred, but can be declared
   var s: String = "abc"
 
+  //- start scala_loop
+  // Loops from 0 to 9
+  // Automatically creates loop value i
+  for (i <- 0 until 10) {
+    println(i)
+  }
+  //- end
+
+  val inVal = 1.U
+
+  //- start scala_loop_gen
+  val shiftReg = RegInit(0.U(8.W))
+
+  shiftReg(0) := inVal
+
+  for (i <- 1 until 8) {
+    shiftReg(i) := shiftReg(i-1)
+  }
+  //- end
+
+  //- start scala_condition
+  for (i <- 0 until 10) {
+    if (i%2 == 0) {
+      println(i + " is even")
+    } else {
+      println(i + " is odd")
+    }
+  }
+  //- end
 }
