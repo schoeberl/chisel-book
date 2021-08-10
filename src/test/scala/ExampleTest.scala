9214c956ec8c3d65109b44b3b7ef64d5958d161f
Martin Schoeberl <martin@jopdesign.com>
2021-08-10 14:41:03 +0200

    testing: update the ScalaTest example

--- src/test/scala/ExampleTest.scala
+++ src/test/scala/ExampleTest.scala
@@ -7,5 +7,11 @@ class ExampleTest extends FlatSpec with Matchers {
     val j = 3
     i + j should be (5)
   }
+
+  "Integers" should "multiply" in {
+    val a = 3
+    val b = 4
+    a * b should be (12)
+  }
 }
 //- end
