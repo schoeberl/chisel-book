abe41b8e82669cec133101f54ce177d075d03c4d
Martin Schoeberl <martin@jopdesign.com>
2022-05-19 17:02:56 +0200

    More arbitration vesions

--- src/main/scala/arbiter.scala
+++ src/main/scala/arbiter.scala
@@ -1,5 +1,7 @@
 import chisel3._
 
+import chisel3.util._
+
 class Arbiter3 extends Module {
   val io = IO(new Bundle{
     val request = Input(UInt(3.W))
@@ -20,7 +22,54 @@ class Arbiter3 extends Module {
   //- end
 
   io.grant := grant.asUInt
+}
 
+class Arbiter3Direct extends Module {
+  val io = IO(new Bundle{
+    val request = Input(UInt(3.W))
+    val grant = Output(UInt(3.W))
+  })
+
+  val request = io.request
+
+  //- start arbiter3-direct
+  val grant = WireDefault("b0000".U(3.W))
+  switch (request) {
+    is ("b000".U) { grant := "b000".U}
+    is ("b001".U) { grant := "b001".U}
+    is ("b010".U) { grant := "b010".U}
+    is ("b011".U) { grant := "b001".U}
+    is ("b100".U) { grant := "b100".U}
+    is ("b101".U) { grant := "b001".U}
+    is ("b110".U) { grant := "b010".U}
+    is ("b111".U) { grant := "b001".U}
+  }
+  //- end
+  io.grant := grant
+}
+
+class Arbiter3Loop extends Module {
+  val io = IO(new Bundle{
+    val request = Input(UInt(3.W))
+    val grant = Output(UInt(3.W))
+  })
+
+  val request = VecInit(io.request.asBools)
+  val n = request.length
+
+  //- start arbiter3-loop
+  val grant = VecInit.fill(n)(false.B)
+  val notGranted = VecInit.fill(n)(false.B)
+
+  grant(0) := request(0)
+  notGranted(0) := !grant(0)
+  for (i <- 1 until n) {
+    grant(i) := request(i) && notGranted(i-1)
+    notGranted(i) := !grant(i) && notGranted(i-1)
+  }
+  //- end
+
+  io.grant := grant.asUInt
 }
 
 /*
@@ -29,6 +78,9 @@ This does not work with plain UInts, as Chisel has parital assignment issues
   val grant = WireDefault(0.U(3.W))
 
   grant(0) := request(0)
-  grant(1) := request(1) & ~grant(0)
-  grant(2) := request(2) & ~grant(1)
+  notGranted(0) := ~grant(0)
+  grant(1) := request(1) & notGranted(0)
+  notGranted(1) := ~grant(1) & notGranted(0)
+  grant(2) := request(2) & notGranted(1)
+
  */
--- src/test/scala/ArbiterTest.scala
+++ src/test/scala/ArbiterTest.scala
@@ -4,21 +4,44 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class ArbiterTest extends AnyFlatSpec with ChiselScalatestTester {
 
+  def check(i: Int) = {
+    if ((i & 1) == 1) {
+      "b001".U
+    } else if ((i & 3) == 2) {
+      "b010".U
+    } else if (i == 4) {
+      "b100".U
+    } else {
+      "b000".U
+    }
+  }
+
   "Arbiter3" should "pass" in {
     test(new Arbiter3()) { dut =>
       for (i <- 0 until 8) {
-        println(s"$i")
         dut.io.request.poke(i.U)
         dut.clock.step()
-        if ((i & 1) == 1) {
-          dut.io.grant.expect("b001".U)
-        } else if ((i & 3) == 2) {
-          dut.io.grant.expect("b010".U)
-        } else if (i == 4) {
-          dut.io.grant.expect("b100".U)
-        } else {
-          dut.io.grant.expect("b000".U)
-        }
+        dut.io.grant.expect(check(i))
+      }
+    }
+  }
+
+  "Arbiter3Direct" should "pass" in {
+    test(new Arbiter3Direct()) { dut =>
+      for (i <- 0 until 8) {
+        dut.io.request.poke(i.U)
+        dut.clock.step()
+        dut.io.grant.expect(check(i))
+      }
+    }
+  }
+
+  "Arbiter3Loop" should "pass" in {
+    test(new Arbiter3Loop()) { dut =>
+      for (i <- 0 until 8) {
+        dut.io.request.poke(i.U)
+        dut.clock.step()
+        dut.io.grant.expect(check(i))
       }
     }
   }
