8c37abfa9d7bbefa476bb006031ba583d7668d90
Martin Schoeberl <martin@jopdesign.com>
2022-07-19 13:04:51 +0300

    rewriting of Chapter 9

--- src/test/scala/PopCountTest.scala
+++ src/test/scala/PopCountTest.scala
@@ -8,7 +8,7 @@ class PopCountTest extends AnyFlatSpec with ChiselScalatestTester {
     test(new PopCount) { dut =>
       dut.io.din.poke(0xac.U)
       dut.io.dinValid.poke(true.B)
-      dut.clock.step(9)
+      dut.clock.step(12)
       dut.io.popCnt.expect(4.U)
     }
   }
