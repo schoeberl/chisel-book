5c8d459e0759bada70537d0530c7765ab5fe27ce
Martin Schoeberl <martin@jopdesign.com>
2022-08-10 17:19:21 +0200

    Have a simpler arbiter

--- src/main/scala/ArbiterTree.scala
+++ src/main/scala/ArbiterTree.scala
@@ -1,27 +1,29 @@
 import chisel3._
 import chisel3.util._
 
-// Only one will be ready, as we cannot take two values
-// This would need a shadow register, a reasonable optimisation
-// Without optimisation one channel can only take one data every 2 clock cycles
 
 
 //- start fun_arbiter_head
-class ArbiterTree[T <: Data: Manifest](n: Int, private val gen: T) extends Module {
+class Arbiter[T <: Data: Manifest](n: Int, private val gen: T) extends Module {
   val io = IO(new Bundle {
     val in = Flipped(Vec(n, new DecoupledIO(gen)))
     val out = new DecoupledIO(gen)
   })
   //- end
+}
+
+// Only one will be ready, as we cannot take two values
+// This would need a shadow register, a reasonable optimisation
+// Without optimisation one channel can only take one data every 2 clock cycles
+class ArbiterTree[T <: Data: Manifest](n: Int, private val gen: T) extends Arbiter(n, gen) {
 
   //- start fun_arbiter
-  def arbitrateTwo(a: DecoupledIO[T], b: DecoupledIO[T]) = {
+  def arbitrateFair(a: DecoupledIO[T], b: DecoupledIO[T]) = {
 
     val idleA :: idleB :: hasA :: hasB :: Nil = Enum(4)
     val regData = Reg(gen)
     val regState = RegInit(idleA)
     val out = Wire(new DecoupledIO(gen))
-
     a.ready := regState === idleA
     b.ready := regState === idleB
     out.valid := (regState === hasA || regState === hasB)
@@ -59,7 +61,53 @@ class ArbiterTree[T <: Data: Manifest](n: Int, private val gen: T) extends Modul
   }
   //- end
 
+  io.out <> io.in.reduceTree((a, b) => arbitrateFair(a, b))
+}
+
+
+class ArbiterSimpleTree[T <: Data: Manifest](n: Int, private val gen: T) extends Arbiter(n, gen) {
+
+  //- start fun_arbiter_simple
+  def arbitrateSimp(a: DecoupledIO[T], b: DecoupledIO[T]) = {
+
+    val regData = Reg(gen)
+    val regEmpty = RegInit(true.B)
+    val regReadyA = RegInit(false.B)
+    val regReadyB = RegInit(false.B)
+
+    val out = Wire(new DecoupledIO(gen))
+
+    when (a.valid & regEmpty & !regReadyB) {
+      regReadyA := true.B
+    } .elsewhen (b.valid & regEmpty & !regReadyA) {
+      regReadyB := true.B
+    }
+    a.ready := regReadyA
+    b.ready := regReadyB
+
+    when (regReadyA) {
+      regData := a.bits
+      regEmpty := false.B
+      regReadyA := false.B
+    }
+    when (regReadyB) {
+      regData := b.bits
+      regEmpty := false.B
+      regReadyB := false.B
+    }
+
+    out.valid := !regEmpty
+    when (out.ready) {
+      regEmpty := true.B
+    }
+
+    out.bits := regData
+    out
+  }
+  //- end
+
   //- start fun_arbiter_end
-  io.out <> io.in.reduceTree((a, b) => arbitrateTwo(a, b))
+  io.out <> io.in.reduceTree((a, b) => arbitrateSimp(a, b))
 }
 //- end
+
--- src/test/scala/ArbiterTreeTest.scala
+++ src/test/scala/ArbiterTreeTest.scala
@@ -4,46 +4,61 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class ArbiterTreeTest extends AnyFlatSpec with ChiselScalatestTester {
 
-  behavior of "ArbiterTree"
-
-  it should "pass" in {
-    test(new ArbiterTree(4, UInt(8.W))).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
-    // test(new Arbiter(4, UInt(8.W))) { dut =>
-      for (i <- 0 until 4) {
-        dut.io.in(i).valid.poke(false.B)
-      }
-      dut.io.out.ready.poke(false.B) // Keep the output till we read it
-      dut.io.in(2).valid.poke(true.B)
-      dut.io.in(2).bits.poke(2.U)
-      while(!dut.io.in(2).ready.peek().litToBoolean) {
-        dut.clock.step()
-      }
+  def testBasic[T <: Arbiter[_ <: UInt]](dut: T) = {
+    for (i <- 0 until 4) {
+      dut.io.in(i).valid.poke(false.B)
+    }
+    dut.io.out.ready.poke(false.B) // Keep the output till we read it
+    dut.io.in(2).valid.poke(true.B)
+    dut.io.in(2).bits.poke(2.U)
+    while(!dut.io.in(2).ready.peek().litToBoolean) {
       dut.clock.step()
-      dut.io.in(2).valid.poke(false.B)
-      dut.clock.step(10)
-      // disable for now to avoid Travis issue.
-      dut.io.out.bits.expect(2.U)
+    }
+    dut.clock.step()
+    dut.io.in(2).valid.poke(false.B)
+    dut.clock.step(10)
+    dut.io.out.bits.expect(2.U)
+  }
+
+  "ArbiterTree" should "pass" in {
+    test(new ArbiterTree(4, UInt(8.W))) { dut =>
+      testBasic(dut)
+    }
+  }
+
+  "ArbiterSimpleTree" should "pass" in {
+    test(new ArbiterSimpleTree(4, UInt(8.W))) { dut =>
+      testBasic(dut)
     }
   }
 
   // TODO: finish the test
-  it should "be fair" in {
-    test(new ArbiterTree(5, UInt(16.W))).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
-      for (i <- 0 until 5) {
-        dut.io.in(i).valid.poke(true.B)
-        dut.io.in(i).bits.poke((i*100).U)
+  def testFair[T <: Arbiter[_ <: UInt]](dut: T) = {
+    for (i <- 0 until 5) {
+      dut.io.in(i).valid.poke(true.B)
+      dut.io.in(i).bits.poke((i*100).U)
+    }
+    // println("Result should be " + List(0, 100, 200, 300, 400).sum)
+    dut.io.out.ready.poke(true.B)
+    dut.clock.step()
+    for (i <- 0 until 40) {
+      if (dut.io.out.valid.peek().litToBoolean) {
+        println(dut.io.out.bits.peek().litValue)
       }
-      // println("Result should be " + List(0, 100, 200, 300, 400).sum)
-      dut.io.out.ready.poke(true.B)
       dut.clock.step()
-      for (i <- 0 until 40) {
-        if (dut.io.out.valid.peek().litToBoolean) {
-          println(dut.io.out.bits.peek().litValue)
-        }
-        dut.clock.step()
-      }
     }
   }
+
+  "ArbiterTree" should "be fair" in {
+    test(new ArbiterTree(5, UInt(16.W))) { dut => testFair(dut) }
+  }
+
+  /*
+  "ArbiterSimpleTree" should "be fair" in {
+    test(new ArbiterSimpleTree(5, UInt(16.W))) { dut => testFair(dut) }
+  }
+
+   */
 
   /*
   // This depends on an add in the node, find a better way to do this
