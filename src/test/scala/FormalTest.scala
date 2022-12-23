2b865d8c47cfd878c602989ab5f29bc2cedaea45
Martin Schoeberl <martin@jopdesign.com>
2022-12-23 16:30:14 +0100

    Minor edits

--- src/test/scala/FormalTest.scala
+++ src/test/scala/FormalTest.scala
@@ -24,9 +24,36 @@ class FormalSimpleTest(width: Int) extends Module {
   assert(m.io.y === resUInt)
 }
 
+class PastTest() extends Module {
+  val io = IO(new Bundle() {
+    val in = Input(UInt(8.W))
+    // val out = Output(UInt(8.W))
+  })
+
+  val reg = RegNext(RegNext(io.in))
+  assert(reg === past((past(io.in))))
+}
+
+class AssumeTest extends Module {
+  val in = IO(Input(UInt(8.W)))
+  val out = IO(Output(UInt(8.W)))
+  out := in + 1.U
+  assume(in > 3.U && in < 255.U)
+  assert(out > 4.U)
+  assert(out <= 255.U)
+}
+
 class FormalTest extends AnyFlatSpec with ChiselScalatestTester with Formal {
 
   "FormalSimple" should "pass" in {
     verify(new FormalSimpleTest(8), Seq(BoundedCheck(1)))
+  }
+
+  "PastTest" should "pass" in {
+    verify(new PastTest(), Seq(BoundedCheck(5), WriteVcdAnnotation))
+  }
+
+  "AssumeTest" should "pass" in {
+    verify(new AssumeTest(), Seq(BoundedCheck(5), WriteVcdAnnotation))
   }
 }
