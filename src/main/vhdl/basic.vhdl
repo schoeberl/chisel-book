f8c7da8195e3851280a97bb19a736812df3208b6
Martin Schoeberl <martin@jopdesign.com>
2023-03-10 10:11:39 +0100

    slides and other changes

--- src/main/vhdl/basic.vhdl
+++ src/main/vhdl/basic.vhdl
@@ -12,3 +12,58 @@ architecture behavioral of adder is
 begin
   sum <= a + b;
 end architecture;
+
+-- the following code was all written by Copilot!
+
+-- adder_tb.vhd
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity adder_tb is
+end entity;
+
+architecture behavioral of adder_tb is
+  signal a, b, sum : std_logic_vector(7 downto 0);
+begin
+    uut : entity work.adder
+        port map (
+        a => a,
+        b => b,
+        sum => sum
+        );
+
+    a <= "00000000";
+    b <= "00000000";
+    wait for 10 ns;
+    assert sum = "00000000" report "0 + 0 = 0" severity note;
+
+    a <= "00000000";
+    b <= "00000001";
+    wait for 10 ns;
+    assert sum = "00000001" report "0 + 1 = 1" severity note;
+
+    a <= "00000001";
+    b <= "00000000";
+    wait for 10 ns;
+    assert sum = "00000001" report "1 + 0 = 1" severity note;
+
+    a <= "00000001";
+    b <= "00000001";
+    wait for 10 ns;
+    assert sum = "00000010" report "1 + 1 = 2" severity note;
+
+    a <= "11111111";
+    b <= "00000001";
+    wait for 10 ns;
+    assert sum = "00000000" report "255 + 1 = 0" severity note;
+
+    a <= "11111111";
+    b <= "11111111";
+    wait for 10 ns;
+    assert sum = "11111110" report "255 + 255 = 510" severity note;
+
+    a <= "11111111";
+    b <= "11111111";
+    wait for 10 ns;
+    assert sum = "11111111" report "255 + 255 = 510" severity note;
+    end architecture;
