17346f5b19064953abacb3bd18cde0e9a43ef4df
Martin Schoeberl <martin@jopdesign.com>
2025-10-18 15:58:43 +0100

    slides: work on week 7

--- src/main/scala/fifo/fifo.scala
+++ src/main/scala/fifo/fifo.scala
@@ -16,7 +16,7 @@ class FifoIO[T <: Data](private val gen: T) extends Bundle {
 abstract class Fifo[T <: Data](gen: T, val depth: Int) extends Module {
   val io = IO(new FifoIO(gen))
 
-  assert(depth > 0, "Number of buffer elements needs to be larger than 0")
+  require(depth > 0, "Number of buffer elements needs to be larger than 0")
 }
 //- end
 
